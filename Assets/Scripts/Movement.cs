aef4fbdf9408f9877d45f53ca1c17d7938de0e3f
CamDHall <camerondylan.h@gmail.com>
2017-09-13 18:09:10 -0400

    Basic movement except jump

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float movementSpeed;
+    public static Movement Instance;
+    public bool colliding = false;
+
+	void Start () {
+        Instance = this;
+		if( movementSpeed <= 0)
+        {
+            movementSpeed = 1;
+        }
+	}
+
+	void Update () {
+
+	}
+
+    private void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * (Time.deltaTime * movementSpeed));
+        }
+
+        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * (Time.deltaTime * movementSpeed));
+        }
+    }
+
+    public void Jump()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        colliding = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        colliding = false;
+    }
+}
---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+    float fireTimer = 0;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.RightShift))
+        {
+            if (Time.time > fireTimer || fireTimer == 0)
+            {
+                if(!Movement.Instance.colliding)
+                    Shoot();
+                else
+
+            }
+        }
+	}
+
+    void Shoot()
+    {
+        Vector2 Pos = new Vector2(Movement.Instance.transform.position.x, Movement.Instance.transform.position.y - 0.5f);
+        var bullet = Instantiate(bulletPrefab, Pos, Quaternion.identity);
+        fireTimer = Time.time + 2;
+    }
+}
